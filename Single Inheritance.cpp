adcf33fde78a711df2787508f929528d9a525a96
Ishan2103 <ishanextra21@gmail.com>
2025-09-28 23:14:24 +0530

    Create Single Inheritance.cpp

---
+++ Single Inheritance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    string type="Dog";
+    void sound(){
+        cout<<"Barks\n";
+    }
+};
+class Pet: public Animal {
+public:
+    string name="tommy";
+};
+int main(){
+    Pet p1;
+    cout<<"Type: "<<p1.type<<endl;
+    cout<<"Name: "<<p1.name<<endl;
+    p1.sound();
+    return 0;
+}
