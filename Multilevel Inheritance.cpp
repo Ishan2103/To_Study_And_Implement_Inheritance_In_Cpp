805e26503f3dba5c40665bd43dfe848963159133
Ishan2103 <ishanextra21@gmail.com>
2025-09-28 23:06:28 +0530

    Create Multilevel Inheritance.cpp

---
+++ Multilevel Inheritance.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class LivingBeing{
+public:
+    void breed(){
+        cout<<"French Bulldog"<<endl;
+    }
+};
+class Animal:public LivingBeing {
+public:
+    void eat(){
+        cout<<"Animals eat food"<<endl;
+    }
+};
+class Dog:public Animal {
+public:
+    void colour(){
+        cout<<"black"<<endl;
+    }
+};
+int main(){
+    Dog myDog;
+    myDog.breed();  
+    myDog.eat();                              
+    cout<<"Dog colour"<<endl;
+    myDog.colour();                              
+    return 0;
+}
